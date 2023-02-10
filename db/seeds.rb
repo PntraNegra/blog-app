dae33f3e9d5f07efd3baf4dbe96f8b94cf1be4ff
Brian Cervantes <briancervantes@Brians-MacBook-Air.local>
2023-02-10 14:17:35 -0800

    create files password digest and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+usernames = ["brian_c", "joseph_p", "jeremy_f"]
+emails = ["brian@gmail.com", "joseph@gmail.com", "jeremy@gmail.com"]
+passwords = ["password1", "password2", "password3" ]
+
+brian = User.create!(username: usernames[0], password: passwords[0], email: emails[0])
+joseph = User.create!(username: usernames[1], password: passwords[1], email: emails[1])
+jeremy = User.create!(username: usernames[2], password: passwords[2], email: emails[2])
+
+brian.articles.create!([
+    {
+        title: "First article title",
+        description: "First article description."
+    },
+    {
+        title: "Another one",
+        description: "Let's write some random stuff here."
+    },
+    {
+        title: "And another article title",
+        description: "I like to eat my coffee and drink my chicken."
+    }
+])
+
+joseph.articles.create!([
+    {
+        title: "Hi there",
+        description: "Hello world! my name is joseph."
+    },
+    {
+        title: "Test test test",
+        description: "Testing testing one two three one two three"
+    },
+    {
+        title: "And another article title",
+        description: "hi, i want to eat."
+    }
+])
+
+jeremy.articles.create!([
+    {
+        title: "Jeremy here",
+        description: "Hey everyone this is Jeremy."
+    },
+    {
+        title: "Foody article",
+        description: "Hi everyone, I like food, do you like food too?"
+    },
+    {
+        title: "Bye Bye Bye",
+        description: "I like that song bye bye bye, but I forgot who sings it..."
+    }
+])
