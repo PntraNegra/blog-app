70b32d51fe1ca08d1850dfc95aaea8f2d802afdc
Brian Cervantes <briancervantes@Brians-Air.lan>
2023-02-10 17:44:56 -0800

    Add users signup view and create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-
+  resources :users, except: [:new]
   resources :articles
+
   root "pages#home"
   get 'pages/home'
   get 'pages/about'
+
+  get 'signup', to: 'users#new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
