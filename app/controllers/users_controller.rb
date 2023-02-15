c02d2478f150c89226ca0ddb5c92a6dfe8b08dfc
Brian Cervantes <briancervantes@Brians-MacBook-Air.local>
2023-02-15 13:48:36 -0800

    restrict actions for editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :require_user, only: %i[ edit update ]
+  before_action :require_same_user, only: %i[ edit update]
 
   # GET /users or /users.json
   def index
@@ -70,4 +72,11 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only edit your own account"
+        redirect_to @user
+      end
+    end  
 end
